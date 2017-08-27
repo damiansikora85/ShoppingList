fc512884187e357511607776acfdba6a054b1d79
damian.sikora <dsikora85@gmail.com>
2017-08-27 18:36:52 +0200

    Added UITest project

---
+++ ShoppingListTest/AppInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace ShoppingListTest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.ApkFile("ShoppingList.Android.apk").StartApp();
+                //return ConfigureApp.Android.StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+

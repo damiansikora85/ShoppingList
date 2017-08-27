b852c03a0f07e50bd90637ab7a9305b660ff9b48
damian.sikora <dsikora85@gmail.com>
2017-08-27 18:06:50 +0200

    Added shopping list model

---
+++ ShoppingList/ShoppingList/Services/ShoppingListManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Services
+{
+    public class ShoppingListManager
+    {
+        public List<string> SavedLists { get; private set; }
+        private static ShoppingListManager instance;
+
+        public static ShoppingListManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ShoppingListManager();
+
+                return instance;
+            }
+        }
+
+        private ShoppingListManager()
+        {
+            SavedLists = new List<string>()
+            {
+                "List1",
+                "List2",
+                "List3",
+                "List4",
+                "List5"
+            };
+        }
+
+    }
+}
---
+++ ShoppingList/ShoppingList/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.ViewModels
+{
+    public class MainPageViewModel : Helpers.ObservableObject
+    {
+        public List<string> ShoppingLists { get; set; }
+
+        public MainPageViewModel()
+        {
+            ShoppingLists = ShoppingList.Services.ShoppingListManager.Instance.SavedLists;
+        }
+
+        public void AddNewShoppingList()
+        {
+
+        }
+    }
+}

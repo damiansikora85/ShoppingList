781eca45ccb1eb8ed9331f657b060a9dfdaa588c
damian.sikora <dsikora85@gmail.com>
2017-08-31 20:36:52 +0200

    Added some test cases

--- ShoppingListTest/Tests.cs
+++ ShoppingListTest/Tests.cs
@@ -30,6 +30,48 @@ namespace ShoppingListTest
         {
             app.Screenshot("First screen.");
         }
+
+        [Test]
+        public void AddEmptyList()
+        {
+            app.WaitForElement(c => c.Marked("NoResourceEntry-0").Text("New Shopping"));
+            app.Tap(c => c.Marked("NoResourceEntry-0"));
+            app.EnterText(e => e.Class("EntryEditText"), "NewList");
+            app.PressEnter();
+            app.Tap(c => c.Marked("NoResourceEntry-0"));
+            Assert.AreEqual(1, app.Query(q => q.Class("ListView").Child()).Length);
+        }
+
+        [Test]
+        public void AddAndRemoveList()
+        {
+            app.WaitForElement(c => c.Marked("NoResourceEntry-0").Text("New Shopping"));
+            app.Tap(c => c.Marked("NoResourceEntry-0"));
+            app.EnterText(e => e.Class("EntryEditText"), "NewList");
+            app.PressEnter();
+            app.Tap(c => c.Marked("NoResourceEntry-0"));
+            Assert.AreEqual(1, app.Query(q => q.Class("ListView").Child()).Length);
+            app.Tap(c => c.Class("AppCompatButton"));
+            Assert.AreEqual(0, app.Query(q => q.Class("ListView").Child()).Length);
+        }
+
+        [Test]
+        public void AddListWithItems()
+        {
+            int itemsNum = 7;
+            app.WaitForElement(c => c.Marked("NoResourceEntry-0").Text("New Shopping"));
+            app.Tap(c => c.Marked("NoResourceEntry-0"));
+            app.EnterText(e => e.Class("EntryEditText"), "NewList");
+            app.PressEnter();
+
+            for (int i = 0; i < itemsNum; i++)
+            {
+                app.EnterText(c => c.Class("EntryEditText").Index(1), "item"+i);
+                app.PressEnter();
+            }
+
+            Assert.AreEqual(itemsNum, app.Query(q => q.Class("ListView").Child()).Length);
+        }
     }
 }
 
